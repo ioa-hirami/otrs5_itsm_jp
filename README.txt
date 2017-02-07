b1160ef169f08b0190426943be4af545ed9eb0cc
Tomohisa Hirami <hirami@io-architect.com>
2017-02-07 09:56:08 +0900

    5.0.16に更新

--- README.txt
+++ README.txt
@@ -1,24 +1,28 @@
-OTRS5 HelpdeskにITSM関連モジュールを追加したものです。
+OTRS5 Helpdeskに追加モジュール及びITSM関連モジュールを追加したものです。
 
 各モジュールのバージョンは以下の通りです。
-OTRS 5.0.14
-GeneralCatalog 5.0.14
-ImportExport 5.0.14
-ITSMChangeManagement 5.0.14
-ITSMConfigurationManagement 5.0.14
-ITSMCore 5.0.14
-ITSMIncidentProblemManagement 5.0.14
-ITSMServiceLevelManagement 5.0.14
-OTRSMasterSlave 5.0.2
+
+OTRS 5.0.16
+
+- ITSM -
 Survey 5.0.2
 SystemMonitoring 5.0.1
-TimeAccounting 5.0.4
-
+TimeAccounting 5.0.6
+FAQ 5.0.7
 OTRSAppointmentCalendar 5.0.1
+
+- ITSM -
+GeneralCatalog 5.0.16
+ImportExport 5.0.16
+ITSMChangeManagement 5.0.16
+ITSMConfigurationManagement 5.0.16
+ITSMCore 5.0.16
+ITSMIncidentProblemManagement 5.0.16
+ITSMServiceLevelManagement 5.0.16
+OTRSMasterSlave 5.0.6
 
 
 OTRS5sの新機能であるカレンダーは本イメージに追加しています。
-FAQはイメージ作成時点で公開サーバから消えていましたので一時的に追加していません
 
 実行は以下のようにしてください。
 docker run -d -p <ポート>:80 docker.io/ioarchitectjp/otrs5_itsm_jp
--- instpkg.sh
+++ instpkg.sh
@@ -2,19 +2,19 @@
 
 CMD=/opt/otrs/bin/otrs.Console.pl
 
-#$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:FAQ-4.0.5.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:FAQ-5.0.7.opm
 $CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:Survey-5.0.2.opm
 $CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:SystemMonitoring-5.0.1.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:TimeAccounting-5.0.4.opm
-#$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSMasterSlave-5.0.2.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:TimeAccounting-5.0.6.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSMasterSlave-5.0.6.opm
 
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:GeneralCatalog-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ImportExport-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMCore-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMChangeManagement-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMConfigurationManagement-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMIncidentProblemManagement-5.0.14.opm
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMServiceLevelManagement-5.0.14.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:GeneralCatalog-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ImportExport-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMCore-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMChangeManagement-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMConfigurationManagement-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMIncidentProblemManagement-5.0.16.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/itsm/packages5/:ITSMServiceLevelManagement-5.0.16.opm
 
-$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSAppointmentCalendar-5.0.1.opm
+$CMD Admin::Package::Install http://ftp.otrs.org/pub/otrs/packages/:OTRSAppointmentCalendar-5.0.2.opm
 
