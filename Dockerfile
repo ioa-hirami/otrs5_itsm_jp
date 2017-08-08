f25e61f9503b65ed1672856d7d30f6dc42dd7f4f
Tomohisa Hirami <hirami@io-architect.com>
2017-08-08 18:47:08 +0900

    DockerHubでのビルドに失敗する場合があったので修正

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 FROM ioarchitectjp/otrs5_helpdesk_jp:latest
 MAINTAINER Tomohisa Hirami<hirami@io-architect.com>
 
-COPY instpkg.sh /
-COPY inst.sh /
+COPY instpkg.sh inst.sh /
 RUN chmod 775 /inst.sh /instpkg.sh
 RUN /inst.sh
--- inst.sh
+++ inst.sh
@@ -1,18 +1,28 @@
 #! /usr/bin/sh
 
-rm /var/run/mysqld/mysqld.pid
+rm /var/lib/mysql/mysql.sock.lock
 rm /var/log/mysqld.log
+touch /var/log/mysqld.log
+chown mysql.mysql /var/log/mysqld.log
 
-/usr/sbin/mysqld --user=root &
+/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --user=root --log-error=/var/log/mysqld.log &
 echo waiting to start mysqld..
-sleep 20
-echo done.
 
-ps augx
-cat /var/log/mysqld.log
-
+while :
+do
+  ps augx | grep mysqld > /dev/null 2>&1
+  if [ $? = 0 ]; then
+    echo "OK."
+    break
+  fi
+  echo "waiting.."
+  sleep 1
+done
 
 yum install -y perl-DateTime
+
+#ps augx
+#cat /var/log/mysqld.log
 
 echo Installing ITSM packages..
 su -c /instpkg.sh otrs
