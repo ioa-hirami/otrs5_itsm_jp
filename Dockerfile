8be8f1b01fe4c3c171476dca5a488e1ec9d6de2d
Tomohisa Hirami <hirami@io-architect.com>
2017-08-09 11:48:35 +0900

    mysql初期化スクリプトを実行するように修正

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM ioarchitectjp/otrs5_helpdesk_jp:latest
 MAINTAINER Tomohisa Hirami<hirami@io-architect.com>
 
-COPY instpkg.sh inst.sh /
-RUN chmod 775 /inst.sh /instpkg.sh
+COPY instpkg.sh inst.sh mysql_init.sh /
+RUN chmod 775 /inst.sh /instpkg.sh /mysql_init.sh
 RUN /inst.sh
--- inst.sh
+++ inst.sh
@@ -5,19 +5,13 @@ rm /var/log/mysqld.log
 touch /var/log/mysqld.log
 chown mysql.mysql /var/log/mysqld.log
 
-/usr/sbin/mysqld --pid-file=/var/run/mysqld/mysqld.pid --datadir=/var/lib/mysql --user=root --log-error=/var/log/mysqld.log &
-echo waiting to start mysqld..
+rm /root/.my.cnf
 
-while :
-do
-  ps augx | grep `cat /var/run/mysqld/mysqld.pid` > /dev/null 2>&1
-  if [ $? = 0 ]; then
-    echo "OK."
-    break
-  fi
-  echo "waiting.."
-  sleep 1
-done
+rm -rf /var/lib/mysql
+mkdir -p /var/lib/mysql
+chown mysql.mysql /var/lib/mysql
+
+/mysql_init.sh
 
 yum install -y perl-DateTime
 
